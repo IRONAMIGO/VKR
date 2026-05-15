48452c075760510e62bf82f5023bc00506f4a9c7
Aleksandr Zdr <57704794+zdr3zdr@users.noreply.github.com>
2026-05-15 10:12:32 +0300

    CORS_URL

--- front/src/api/hey-api.ts
+++ front/src/api/hey-api.ts
@@ -2,5 +2,5 @@ import type { CreateClientConfig } from '@/api/client/client.gen';
 
 export const createClientConfig: CreateClientConfig = (config) => ({
     ...config,
-    baseUrl: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000',
+    baseUrl: '/api',
 });
