f24519b619a6f13f56f1b22db2cb39141f9c9f12
Aleksandr Zdr <57704794+zdr3zdr@users.noreply.github.com>
2026-05-15 11:34:59 +0300

    hey-api.ts

--- front/src/api/hey-api.ts
+++ front/src/api/hey-api.ts
@@ -1,5 +1,7 @@
 import type { CreateClientConfig } from '@/api/client/client.gen';
 
+console.log('✅ hey-api.ts loaded, baseUrl = /api');
+
 export const createClientConfig: CreateClientConfig = (config) => ({
     ...config,
     baseUrl: '/api',
